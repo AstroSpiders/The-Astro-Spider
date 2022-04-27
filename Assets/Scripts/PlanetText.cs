15f395be905dc0933fe1f7619e6be7066c4ed8b6
DabuAlexandru <adabu34@gmail.com>
2022-04-28 00:09:40 +0300

    now the text alpha changes depending on the distance from the camera to the planet - when we get close to the planet the text disappears

--- Assets/Scripts/PlanetText.cs
+++ Assets/Scripts/PlanetText.cs
@@ -8,11 +8,17 @@ public class PlanetText : MonoBehaviour
     public int PlanetIndex;
     [SerializeField]
     private float _distanceToCamera;
+    [SerializeField]
+    private float _fadingDistanceStart;
+    [SerializeField]
+    private float _fadingDistanceEnd;
+    private float _fadingInterval;
     private TMP_Text _planetText;
     void Start()
     {
         _planetText = GetComponentInChildren<TMP_Text>();
         _planetText.SetText("#" + (PlanetIndex + 1).ToString());
+        _fadingInterval = _fadingDistanceStart - _fadingDistanceEnd;
     }
 
     void Update()
@@ -24,5 +30,7 @@ public class PlanetText : MonoBehaviour
         _planetText.transform.position = transform.position + toCamera * (radius + _distanceToCamera);
         _planetText.transform.LookAt(Camera.transform.position, Camera.transform.up);
         _planetText.transform.Rotate(0.0f, 180.0f, 0.0f);
+        float distance = Vector3.Distance(Camera.transform.position, transform.position);
+        _planetText.alpha = Mathf.Clamp((distance - _fadingDistanceEnd) / _fadingInterval, 0.0f, 1.0f);
     }
 }
