b10aef0c730d1d0e7743720c3d0f27ba11e7a11a
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-04-30 00:14:02 +0300

    Improved planet text

--- Assets/Scripts/PlanetText.cs
+++ Assets/Scripts/PlanetText.cs
@@ -3,34 +3,43 @@ using TMPro;
 
 public class PlanetText : MonoBehaviour
 {
-    public Camera Camera;
+    public Camera         Camera;
+    public int            PlanetIndex;
+    
+    [SerializeField]
+    private float         _distanceToCamera;
+    [SerializeField]
+    private float         _fadingDistanceStart;
+    [SerializeField]
+    private float         _fadingDistanceEnd;
+    
+    private float         _fadingInterval;
+    private TMP_Text      _planetText;
 
-    public int PlanetIndex;
-    [SerializeField]
-    private float _distanceToCamera;
-    [SerializeField]
-    private float _fadingDistanceStart;
-    [SerializeField]
-    private float _fadingDistanceEnd;
-    private float _fadingInterval;
-    private TMP_Text _planetText;
-    void Start()
+    private void Start()
     {
         _planetText = GetComponentInChildren<TMP_Text>();
-        _planetText.SetText("#" + (PlanetIndex + 1).ToString());
+        _planetText.SetText((PlanetIndex + 1).ToString());
         _fadingInterval = _fadingDistanceStart - _fadingDistanceEnd;
     }
 
-    void Update()
+    private void Update()
     {
         if (Camera is null)
             return;
-        Vector3 toCamera = (Camera.transform.position - transform.position).normalized;
-        float radius = Mathf.Max(transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        _planetText.transform.position = transform.position + toCamera * (radius + _distanceToCamera);
-        _planetText.transform.LookAt(Camera.transform.position, Camera.transform.up);
+
+        var textTransform = transform;
+        var textPosition = textTransform.position;
+        var cameraTransform = Camera.transform;
+        var cameraPosition = cameraTransform.position;
+        var textLocalScale = textTransform.localScale;
+        
+        var toCamera = (cameraPosition - textPosition).normalized;
+        var radius = Mathf.Max(textLocalScale.x, textLocalScale.y, textLocalScale.z);
+        _planetText.transform.position = textPosition + toCamera * (radius + _distanceToCamera);
+        _planetText.transform.LookAt(cameraPosition, cameraTransform.up);
         _planetText.transform.Rotate(0.0f, 180.0f, 0.0f);
-        float distance = Vector3.Distance(Camera.transform.position, transform.position);
+        var distance = Vector3.Distance(cameraPosition, textPosition);
         _planetText.alpha = Mathf.Clamp((distance - _fadingDistanceEnd) / _fadingInterval, 0.0f, 1.0f);
     }
 }
